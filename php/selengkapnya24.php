cc9632ee6d75baa41f7c568c43aad9ef565cf7a3
SayyidIhza11 <151817321+SayyidIhza11@users.noreply.github.com>
2024-09-26 11:12:02 +0700

    Add files via upload

---
+++ php/selengkapnya24.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Selengkapnya</title>
+    <style>
+        .container {
+            display: grid;
+            place-items: center;
+            height: 60vh;
+        }
+        img {
+            max-width: 60%;
+            height: auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <img src="images/selengkapnya24.jpeg" alt="Gambar 24">
+    </div>
+    <style>
+        body {
+            font-family: Poppins, sans-serif;
+        }
+        .container {
+            display: grid;
+            place-items: center;
+            height: 60vh;
+            text-align: center;
+        }
+        .nav-link {
+            display: inline-block;
+            padding: 10px 10px;
+            background-color: #6ea1ff;
+            text-decoration: none;
+            color: #ffffff;
+            border-radius: 35px;
+            margin-top: 5px;
+       }
+       .nav-link:hover {
+            background-color: #5691ff;
+       }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <a class="nav-link" href="galeri.php">Kembali</a>
+    </div>
+</body>
+</html>
